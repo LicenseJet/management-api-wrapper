71fb17ac292e9b3a860f3b4d8a478cacc5d99cd5
Ervin <ervin@sentoweb.com>
2020-02-18 14:58:24 +0200

    Retrieve error message when available

--- LicenseJet/LicenseJetException.php
+++ LicenseJet/LicenseJetException.php
@@ -2,6 +2,8 @@
 
 use GuzzleHttp\Exception\ClientException;
 Use Exception;
+use GuzzleHttp\Exception\RequestException;
+use Throwable;
 
 /**
  * The LicenseJet Exception. This is thrown through the library for both local and remote errors.
@@ -11,6 +13,44 @@ Use Exception;
  */
 Class LicenseJetException extends Exception
 {
+    /**
+     * LicenseJetException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        // Decode response body if possible
+        if ($previous instanceof RequestException)
+        {
+            $response = $previous->getResponse();
+
+            // Response is not available
+            if (!$response)
+            {
+                return;
+            }
+
+            // Decode JSON response if possible
+            if ($response && is_array($response->getHeader('Content-Type')))
+            {
+                // Make sure the decoding is applicable
+                if (in_array('application/json', $response->getHeader('Content-Type')))
+                {
+                    $payload = @json_decode($response->getBody()->getContents());
+
+                    if ($payload && isset($payload->error))
+                    {
+                        $this->message = $payload->error;
+                    }
+                }
+            }
+        }
+    }
+    
     /**
      * Determines if the exception was thrown due to the resource not being found on the service.
      * 
